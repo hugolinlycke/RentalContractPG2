f9c307f54c7fca2d4dca29b9e1d031693802c3af
hugolinlycke <hugo.linlycke@gmail.com>
2020-12-14 11:19:48 +0100

    v1

--- API PY/test.py
+++ API PY/test.py
@@ -1,1 +1,18 @@
-print('Hello World!')
+import flask
+import mysql.connector
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    mydb = mysql.connector.connect(
+        host="193.10.202.77",
+        user="sa",
+        password="TSB100sql"
+    )
+    print(mydb)
+    return "<h1>snopp</h1>"
+
+app.run()
