c77ab5738ffd782582e207ed8fb4e28b13cc65fd
Argon9999 <Argon9999@users.noreply.github.com>
2020-12-15 12:07:05 +0100

    Some CSS work

--- ApartmentRentalSystem/ApartmentRentalSystem/Controllers/LoginController.cs
+++ ApartmentRentalSystem/ApartmentRentalSystem/Controllers/LoginController.cs
@@ -9,6 +9,7 @@ namespace ApartmentRentalSystem.Controllers
 {
     public class LoginController : Controller
     {
+        private string BASE_URL = "http://127.0.0.1:5000/";
         public ActionResult Index()
         {
             return View();
@@ -17,11 +18,20 @@ namespace ApartmentRentalSystem.Controllers
         public ActionResult Index(Models.User inloggning)
         {
             
+            
             return View();
         }
-       public async void CheckLogin()
+        public async void CheckUser()
         {
+            string URL = BASE_URL + "/api/login";
+            HttpClient http = new HttpClient();
+            HttpResponseMessage response = await http.GetAsync(new Uri(URL));
 
+            if (response.IsSuccessStatusCode)
+            {
+
+            }
         }
+      
     }
 }
