78183e4d1ec993d3420a0866e53368b5d2e94fe6
Argon9999 <Argon9999@users.noreply.github.com>
2020-12-15 12:44:55 +0100

    Login Function

--- ApartmentRentalSystem/ApartmentRentalSystem/Controllers/LoginController.cs
+++ ApartmentRentalSystem/ApartmentRentalSystem/Controllers/LoginController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using ApartmentRentalSystem.Models;
 
 namespace ApartmentRentalSystem.Controllers
 {
@@ -15,21 +17,26 @@ namespace ApartmentRentalSystem.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult Index(Models.User inloggning)
+        public ActionResult Index(User inloggning)
         {
             
-            
+            bool result = CheckUser(inloggning).Result;
+            // Here functionall Code. What will happen when we get answers.
             return View();
         }
-        public async void CheckUser()
+        public async Task<bool> CheckUser(User inloggning)
         {
-            string URL = BASE_URL + "/api/login";
+            string URL = BASE_URL + "api/login?username=" + inloggning.Username + "&password=" + inloggning.Password;
             HttpClient http = new HttpClient();
             HttpResponseMessage response = await http.GetAsync(new Uri(URL));
 
             if (response.IsSuccessStatusCode)
             {
-
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
       
