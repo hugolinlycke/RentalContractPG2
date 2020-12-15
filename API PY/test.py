01de9d84fa9cc2aaa42fd130d910c2d520d0a38d
hugolinlycke <hugo.linlycke@gmail.com>
2020-12-15 11:40:57 +0100

    V1
    Implemented firs version of login service. You can also fetch all users. More info on this in the documentation on drive.

--- API PY/test.py
+++ API PY/test.py
@@ -1,18 +1,80 @@
 import flask
-import mysql.connector
+from flask import request, jsonify
+import pyodbc
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+conn = pyodbc.connect(
+    'Driver={SQL Server};'
+    'Server=.\\;'
+    'Database=ApartmentRentalDB;'
+    'Trusted_Connection=yes;'
+)
 
 @app.route('/', methods=['GET'])
 def home():
-    mydb = mysql.connector.connect(
-        host="193.10.202.77",
-        user="sa",
-        password="TSB100sql"
-    )
-    print(mydb)
+        
     return "<h1>snopp</h1>"
+
+#GET ALL USERS
+@app.route('/api/user', methods=['GET'])
+def users():
+    cur = conn.cursor()
+    results = []
+    results = cur.execute('SELECT * FROM [ApartmentRentalDB].[dbo].[User];').fetchall()
+    
+    response = []
+
+    for user in results:
+        response.append(
+            {'Id': user.Id,
+            'Username': user.Username,
+            'Password': user.Password,
+            'Landlord': user.Landlord}
+        )
+        
+    return jsonify(response)
+
+@app.route('/api/user', methods=['POST'])
+def create_user():
+    return "<h1>hej</h1>"
+
+#LOGIN FUNCTION
+#to do fixa 
+@app.route('/api/login', methods=['GET'])
+def login():
+
+    if 'username' in request.args:
+        username = (request.args['username'])
+    else:
+        return 'error no variable'
+
+    if 'password' in request.args:
+        password = (request.args['password'])
+    else:
+        return 'error no variable'
+
+    to_filter = "Username = '" + username + "' AND " + "Password = '" + password + "'"
+    cur = conn.cursor()
+    results = []
+    results = cur.execute('SELECT Username, Password FROM [ApartmentRentalDB].[dbo].[User] WHERE ' + to_filter).fetchall()
+
+    if len(results) == 1:
+        for row in results:
+            if row.Username == username and row.Password == password:
+                response = []
+                response.append(
+                    {"Username": row.Username,
+                    "Password": row.Password}
+                )
+
+                return jsonify(response)
+            else:
+                responseError = {"error": "Username and password did not match"}
+                return jsonify(responseError)
+
+    responseError = {"error": "Could not find user, invalid username or password"}
+    return jsonify(responseError)
 
 app.run()
