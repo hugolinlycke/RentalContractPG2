0d92966ce4d6f1bd5d24056a942558e5ec90b16d
Argon9999 <Argon9999@users.noreply.github.com>
2020-12-22 11:24:39 +0100

    new stuff

--- ApartmentRentalSystem/ApartmentRentalSystem/Controllers/MainController.cs
+++ ApartmentRentalSystem/ApartmentRentalSystem/Controllers/MainController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using ApartmentRentalSystem.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +12,7 @@ namespace ApartmentRentalSystem.Controllers
 {
     public class MainController : Controller
     {
+        private string BASE_URL = "http://127.0.0.1:5000/";
         // GET: Main
         public ActionResult Index()
         {
@@ -17,6 +22,50 @@ namespace ApartmentRentalSystem.Controllers
             }
             return View();
         }
+        [HttpPost]
+        public async Task<ActionResult> Index(User inloggning)
+        {
+
+            string URL = BASE_URL + "api/read/apartments";
+            HttpClient http = new HttpClient();
+            HttpResponseMessage response = await http.GetAsync(new Uri(URL));
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                User activeUser = JsonConvert.DeserializeObject<User>(content);
+
+                if (activeUser.Username != null || activeUser.Password != null)
+                {
+
+                    TempData["SuccessfullLogin"] = "You successfully logged in!";
+                    return RedirectToAction("Index", "Main", inloggning);
+                    //Remove "Login" Text from nav bar then add profile button, if possible
+
+                }
+
+            }
+            else if (!response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode.ToString() == "NotFound")
+                {
+                    ViewBag.Message = String.Format("You could not connect to the API, Please try again later", DateTime.Now.ToString());
+                    return View();
+                }
+                else
+                {
+                    ViewBag.Message = String.Format("You have entered wrong user credentials, Please try again", DateTime.Now.ToString());
+                    return View();
+                }
+
+            }
+
+
+            return View();
+        }
+
+
         public ActionResult Apartment()
         {
             return View();
