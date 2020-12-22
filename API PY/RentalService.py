0e8ac7a3d737e5e5b6b064a5d69389c3347fc141
hugolinlycke <hugo.linlycke@gmail.com>
2020-12-22 11:13:09 +0100

    CRUD for apartments
    And also some different types of read

--- API PY/RentalService.py
+++ API PY/RentalService.py
@@ -18,7 +18,7 @@ def home():
     return "<h1>Main page</h1>"
 
 #GET ALL USERS
-@app.route('/api/read/user', methods=['GET'])
+@app.route('/api/read/users', methods=['GET'])
 def users():
     cur = conn.cursor()
     results = []
@@ -58,6 +58,15 @@ def fetch_one_user(id):
 
 @app.route('/api/create/user', methods=['POST'])
 def create_user():
+
+    try: request.json['Id']
+    except KeyError: return error_page(418, "Id not set")
+
+    try: request.json['Username']
+    except KeyError: return error_page(418, "Username not set")
+
+    try: request.json['Password']
+    except KeyError: return error_page(418, "Password not set")
 
     if request.get_json(force=True)['Username'] != None and request.get_json(force=True)['Landlord'] != None and request.get_json(force=True)['Password'] != None:
         username = request.get_json(force=True)['Username']
@@ -174,12 +183,12 @@ def login():
     if 'username' in request.args:
         username = (request.args['username'])
     else:
-        return 'error no variable'
+        return error_page(418, 'error no variable')
 
     if 'password' in request.args:
         password = (request.args['password'])
     else:
-        return 'error no variable'
+        return error_page(418, 'error no variable')
 
     to_filter = "Username = '" + username + "' AND " + "Password = '" + password + "'"
     cur = conn.cursor()
@@ -203,15 +212,204 @@ def login():
     return error_page(418, "Username and password does not match")
 
 
+@app.route('/api/read/apartments', methods=['GET'])
+def getApartments():
+    cur = conn.cursor()
+    results = []
+    results = cur.execute('SELECT * FROM [ApartmentRentalDB].[dbo].[Apartment];').fetchall()
+        
+    response = []
+
+    for apartment in results:
+        response.append(
+            {'Id': apartment.Id,
+            'Price': apartment.Price,
+            'NumberOfRooms': apartment.NumberOfRooms,
+            'SizeOfApartment': apartment.SizeOfApartment,
+            'Address': apartment.Address,
+            'Location': apartment.Location,
+            'Information': apartment.Information,
+            'LandlordId': apartment.LandlordId,
+            'Active': apartment.Active}
+        )
+        
+    return jsonify(response)
+
+@app.route('/api/read/apartment', methods=['GET'])
+def getApartmentWithId():
+    cur = conn.cursor()
+    to_filter = []
+    if 'id' in request.args:
+        Id = (request.args['id'])
+        if len(to_filter) >= 1:
+            to_filter.append(" AND ")
+        to_filter.append("Id = " + Id) 
+
+    if 'landlordid' in request.args:
+        LandlordId = (request.args['landlordid'])
+        if len(to_filter) >= 1:
+            to_filter.append(" AND ")
+        to_filter.append("LandlordId =" + LandlordId)
+
+    if 'active' in request.args:
+        Active = (request.args['active'])
+        if len(to_filter) >= 1:
+            to_filter.append(" AND ")
+        to_filter.append("Active = '" + str(Active) + "'") 
+
+    strFilter = "WHERE "
+    for x in to_filter:
+        strFilter += x
+
+    if len(to_filter) > 0:
+        results = cur.execute('SELECT * FROM [ApartmentRentalDB].[dbo].[Apartment] ' + strFilter).fetchall()
+        response = []
+
+        if len(results) > 0:
+            for apartment in results:
+                response.append(
+                    {'Id': apartment.Id,
+                    'Price': apartment.Price,
+                    'NumberOfRooms': apartment.NumberOfRooms,
+                    'SizeOfApartment': apartment.SizeOfApartment,
+                    'Address': apartment.Address,
+                    'Location': apartment.Location,
+                    'Information': apartment.Information,
+                    'LandlordId': apartment.LandlordId,
+                    'Active': apartment.Active}
+                )
+            
+            return jsonify(response)
+        else:
+            return error_page(418, "Apartment not found")
+    else:
+        return error_page(418, "Need to use atleast one parameter")
+
+@app.route('/api/create/apartment', methods=['POST'])
+def createApartment():
+
+    data = ['Id', 'Price', 'numberOfRooms', 'sizeOfApartment', 'Address',
+    'Location', 'Information',  'LandlordId', 'Active']
+
+    for x in data:
+        try: request.json[x]
+        except KeyError: return error_page(418, x + " not set")
+
+    if request.json['Address'] != None and request.json['Location'] != None and request.json['LandlordId'] != None and request.json['Active'] != None:
+        Price = request.json['Price']
+        numberOfRooms = request.json['numberOfRooms']
+        sizeOfApartment = request.json['sizeOfApartment']
+        Address = request.json['Address']
+        Location = request.json['Location']
+        Information = request.json['Information']
+        LandlordId = request.json['LandlordId']
+        Active = request.json['Active']
+
+        cur = conn.cursor()
+
+        results = cur.execute("SELECT * FROM [ApartmentRentalDB].[dbo].[User] WHERE Id = " + str(LandlordId)).fetchall()
+
+        if len(results) > 0:
+            cur.execute("INSERT INTO [ApartmentRentalDB].[dbo].[Apartment] (Price, numberOfRooms, sizeOfApartment, Address, Location, Information, LandlordId, Active) VALUES ("+ str(Price)  + ",'" + numberOfRooms + "','" + sizeOfApartment + "','" + Address + "','" + Location + "','" + Information + "'," + str(LandlordId) + ",'" + str(Active) +"');")
+        conn.commit()
+        results1 = cur.execute("SELECT * FROM [ApartmentRentalDB].[dbo].[Apartment] WHERE Id = SCOPE_IDENTITY()").fetchall()
+        if len(results1) > 0:
+            for apartment in results1:
+                response = (
+                     {'Id': apartment.Id,
+                    'Price': apartment.Price,
+                    'NumberOfRooms': apartment.NumberOfRooms,
+                    'SizeOfApartment': apartment.SizeOfApartment,
+                    'Address': apartment.Address,
+                    'Location': apartment.Location,
+                    'Information': apartment.Information,
+                    'LandlordId': apartment.LandlordId,
+                    'Active': apartment.Active}
+                )
+            return jsonify(response)
+        else:
+            return error_page(418, "Landlord don't exist with id: " + LandlordId)
+    else:
+        return error_page(418, "Id, Address, location, landlord id and Active can't be NULL")
+
+@app.route('/api/update/apartment', methods=['PUT'])
+def updateApartment():
+    
+    data = ['Id', 'Price', 'numberOfRooms', 'sizeOfApartment', 'Address',
+    'Location', 'Information',  'LandlordId', 'Active']
+
+    for x in data:
+        try: request.json[x]
+        except KeyError: return error_page(418, x + " not set")
+
+    if request.json['Id'] != None and request.json['Address'] != None and request.json['Location'] != None and request.json['LandlordId'] != None and request.json['Active'] != None:
+        Id = request.json['Id']
+        Price = request.json['Price']
+        numberOfRooms = request.json['numberOfRooms']
+        sizeOfApartment = request.json['sizeOfApartment']
+        Address = request.json['Address']
+        Location = request.json['Location']
+        Information = request.json['Information']
+        LandlordId = request.json['LandlordId']
+        Active = request.json['Active']
+            
+        cur = conn.cursor()
+
+        results = cur.execute("SELECT * FROM [ApartmentRentalDB].[dbo].[Apartment] WHERE Id= " + str(Id)).fetchall()
+
+        if len(results) > 0:
+            cur.execute("UPDATE [ApartmentRentalDB].[dbo].[Apartment] SET Price= " + str(Price) + ", numberOfRooms= '" + numberOfRooms + "', sizeOfApartment ='" + sizeOfApartment + "', Address = '" + Address + "', Location ='" +  Location + "', Information = '" + Information + "', LandlordId = " + str(LandlordId) + ", Active = '" + str(Active) + "' WHERE Id= " + str(Id))
+            conn.commit()
+            results1 = cur.execute("SELECT * FROM [ApartmentRentalDB].[dbo].[Apartment] WHERE Id= " + str(Id)).fetchall()
+            for apartment in results1:
+                response = (
+                    {'Id': apartment.Id,
+                    'Price': apartment.Price,
+                    'NumberOfRooms': apartment.NumberOfRooms,
+                    'SizeOfApartment': apartment.SizeOfApartment,
+                    'Address': apartment.Address,
+                    'Location': apartment.Location,
+                    'Information': apartment.Information,
+                    'LandlordId': apartment.LandlordId,
+                    'Active': apartment.Active}
+                )
+            return jsonify(response)
+        else:
+            return error_page(418, "Could not find apartment")
+
+    else:
+        return error_page(418, "Not all fields are filled out buddy")
+
+@app.route('/api/delete/apartment/<Id>', methods=['DELETE'])
+def deleteApartment(Id):
+
+    cur = conn.cursor()
+
+    results1 = cur.execute('SELECT * FROM [ApartmentRentalDB].[dbo].[Apartment] WHERE Id =' + Id).fetchall()
+
+    if len(results1) > 0:
+        apartmentInInterest = cur.execute("SELECT * FROM [ApartmentRentalDB].[dbo].[Interest] WHERE ApartmentId =" + Id).fetchall()
+        apartmentInOffer = cur.execute("SELECT * FROM [ApartmentRentalDB].[dbo].[RentalOffer] WHERE ApartmentId =" + Id).fetchall()
+
+     
+        if len(apartmentInInterest) > 0:
+            cur.execute('DELETE FROM [ApartmentRentalDB].[dbo].[Interest] WHERE ApartmentId = ' + Id)
+        
+        if len(apartmentInOffer) > 0:
+            cur.execute('DELETE FROM [ApartmentRentalDB].[dbo].[RentalOffer] WHERE ApartmentId = ' + Id)
+
+
+        cur.execute('DELETE FROM [ApartmentRentalDB].[dbo].[Apartment] WHERE Id = ' + Id)
+        conn.commit()
+        return "<h1>Deleted! wow!</h1>"
+    else:
+        return error_page(418, "User not found")
 
 
     #END OF LOGIN
     #NEW SERVICE
 
     #metoder ATT GÖRA
-    #CRUD för apartment
-    #Get specific apartment from apartment ID
-    #Get specific apartment from landlord ID
     
     #CRUD för interest list
     #Get specific interest list from apartmentId
